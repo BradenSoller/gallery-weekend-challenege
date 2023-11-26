4aa63ee264655f1af3a3ab17d5507ba14892f523
Braden Soller <braden.soller18@gmail.com>
2023-11-26 12:33:00 -0600

    tests are passing !! and more fun css

--- server/modules/pool.js
+++ server/modules/pool.js
@@ -9,7 +9,7 @@ if (process.env.NODE_ENV === 'test') {
 const pool = new pg.Pool({
     host: 'localhost',
     port: 5432,
-    database: 'react-gallery'
+    database: databaseName
     // allowExitOnIdle: true 
 })
 
